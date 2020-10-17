b334fa1bc6f2f9796f18f73357b7f05e996305b4
Gene Chatham <genechatham@gmail.com>
2020-10-16 21:29:21 -0700

    add jsPDF

--- src/components/ExportButton.js
+++ src/components/ExportButton.js
@@ -1,15 +1,25 @@
 // ExportButton.js
 
 import React from "react";
-
+import { jsPDF } from "jspdf";
 import "../css/ExportButton.css";
 
 function ExportButton() {
+  const buildPDF = () => {
+    console.log(`building PDF...`);
+    const doc = new jsPDF();
+    doc.text("HELLO WORLD!", 10, 10);
+    doc.save("a4.pdf");
+  };
+
   return (
     <div className="exportButtonWrapper">
-      <div className="exportButton">
+      <div className="exportButton" onClick={buildPDF}>
         make pdf
       </div>
+      <iframe id="testPDF" src="" title="testPDF">
+        testPDFlink?
+      </iframe>
     </div>
   );
 }
